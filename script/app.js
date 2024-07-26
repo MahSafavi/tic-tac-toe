caadafbcf95e974c911b2c708089f94a11201657
MahSafavi <safavi.m@hotmail.com>
2024-07-26 14:48:34 +0200

    start savePlayerConfig

--- script/app.js
+++ script/app.js
@@ -1,6 +1,7 @@
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backDrop = document.getElementById('backdrop');
-
+const formElement = document.querySelector('form');
+const errorOutPut = document.getElementById('config-error');
 
 const editPlayer1Btn = document.getElementById('edit-player-1-btn');
 const editPlayer2Btn = document.getElementById('edit-player-2-btn');
@@ -11,3 +12,5 @@ editPlayer1Btn.addEventListener('click',openPlayerConfig);
 editPlayer2Btn.addEventListener('click',openPlayerConfig);
 cancelConfigBtn.addEventListener('click', closePlayerConfig);
 backDrop.addEventListener('click',closePlayerConfig);
+
+formElement.addEventListener('submit', savePlayerConfig);
--- script/config.js
+++ script/config.js
@@ -8,4 +8,21 @@ function openPlayerConfig(){
 function closePlayerConfig(){
     playerConfigOverlay.style.display='none';
     backDrop.style.display='none';
+    formElement.firstElementChild.classList.remove('error');
+    errorOutPut.textContent="";
+}
+
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData =new FormData(event.target);
+    const enteredPlayerName = formData.get('playername').trim();
+
+    if(!enteredPlayerName){
+        event.target.firstElementChild.classList.add('error'); 
+        errorOutPut.textContent="Please enter a valid name!";
+        return;
+    }
+    
+
 }
